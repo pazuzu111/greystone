4ce85db87b70b94f395fd8cd70e31577b8f54c8a
pazuzu111 <pazuzublue111@gmail.com>
2018-08-13 03:54:24 -0400

    added form submit functionality

--- src/Form.js
+++ src/Form.js
@@ -14,6 +14,49 @@ export default class Form extends Component {
   }
 
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        let expenseSum = 0
+        let income = 0
+
+        this.state.expenses.map(i => {
+            return expenseSum += parseInt(this[`expense${i}`].value)
+        })
+
+        this.state.rentRoll.map((index, el) => {
+            return income += parseInt(this[`rentInput${el}0`]['value'])
+        })
+
+        const data = {
+            income: income * 12, // Annual collected rent (Sum of all rents * 12)
+            expenses: expenseSum, // Total expenses value
+            rate: 3.22, // cap rate
+            noi: this.income - this.expenses, // Net Operating Income (income - expenses)
+            address: {
+                street: this.street.value,
+                city: this.city.value,
+                state: this.state.value,
+                county: this.county.value,
+                zip: this.zip.value
+            }
+        }
+
+        fetch('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbwPGz6uQQS9IW33ASPYlcWaEtRMD8eDAK1ONg7lT2dREXpaSUYh/exec', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(res => {
+            console.log(res)
+        })
+    }
+
+
   render() {
     let address = this.state.address.map((x,i) => {
                     return (
